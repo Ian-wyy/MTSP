0df4fd21b97fdd1f81e7cc867fc6fd705d2eb185
汪奕羽 <14657679+wang_yi_yu@user.noreply.gitee.com>
2024-12-23 18:04:50 +0800

    readmap

--- MTSP.cpp
+++ MTSP.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
 #include <vector>
 
-void readMap(){
-    
+double **readMap(int n) {
+  /* int n = 0;
+  std::cin >> n; */
+  double **map = new double *[n];
+  for (int i = 0; i < n; i++) {
+    map[i] = new double[n];
+  }
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      std::cin >> map[i][j];
+    }
+  }
+  return map;
+}
+
+void testMap(double **map, int n) {
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      std::cout << map[i][j] << " ";
+    }
+    std::cout << std::endl;
+  }
+}
+
+void freeMap(double **map, int n) {
+  for (int i = 0; i < n; i++) {
+    delete[] map[i];
+  }
+  delete[] map;
+}
+
+int main() {
+  int n = 0;
+  std::cin >> n;
+  double **map = readMap(n);
+  testMap(map, n);
+  freeMap(map, n);
 }
