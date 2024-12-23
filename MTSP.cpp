4d6f009b531dc222063583dfe44dbe3bbd4cb255
汪奕羽 <14657679+wang_yi_yu@user.noreply.gitee.com>
2024-12-23 22:18:14 +0800

    one_tsp_0

--- MTSP.cpp
+++ MTSP.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+#define INF 100000
+
 double **readMap(int n) {
   /* int n = 0;
   std::cin >> n; */
@@ -32,10 +34,50 @@ void freeMap(double **map, int n) {
   delete[] map;
 }
 
+double **TSP_dp(double **map, int n) {
+  int m = 1 << (n - 1);
+  double **dp = new double *[n];
+  for (int i = 0; i < n; i++) {
+    dp[i] = new double[m];
+  }
+  for (int i = 0; i < n; i++) {
+    dp[i][0] = map[i][0];
+  }
+
+  for (int j = 1; j < m; j++) {
+    for (int i = 0; i < n; i++) {
+      dp[i][j] = INF;
+      if (((j >> (i - 1)) & 1) == 1) {
+        continue;
+      }
+      for (int k = 1; k < n; k++) {
+        if (((j >> (k - 1)) & 1) == 0) {
+          continue;
+        }
+        if (dp[i][j] > map[i][k] + dp[k][j ^ (1 << (k - 1))]) {
+          dp[i][j] = map[i][k] + dp[k][j ^ (1 << (k - 1))];
+        }
+      }
+    }
+  }
+
+  return dp;
+}
+
+void freeDP(double **dp, int n) {
+  for (int i = 0; i < n; i++) {
+    delete[] dp[i];
+  }
+  delete[] dp;
+}
+
 int main() {
   int n = 0;
   std::cin >> n;
+  int m = 1 << (n - 1);
   double **map = readMap(n);
-  testMap(map, n);
+  double **dp = TSP_dp(map, n);
+  std::cout << dp[0][m - 1] << std::endl;
   freeMap(map, n);
+  freeDP(dp, n);
 }
